9d78b62766e9dc6b886f491f4f7da577c3579b58
Aditya Narayan Singh <adityanarayansingh065@gmail.com>
2017-02-08 11:54:34 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,5 +3,9 @@ console.log('Loaded!');
 //Changing main text
 
 var element = getElementById("text-main");
-
 element.InnerHtml = "HI there";
+
+var img= getElementById("mali");
+img.onClick = function(){
+    img.style.marginRight = "100px";
+};
