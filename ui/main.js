638a46f97049447ef63b5cdd632f62e339683ddb
Aditya Narayan Singh <adityanarayansingh065@gmail.com>
2017-02-08 14:00:21 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -10,6 +10,9 @@ var img= document.getElementById("mali");
 function moveRight(){
     marginRight = marginRight + 2;
     img.style.marginRight = marginRight + "px";
+    if(img.style.marginLeft === 0){
+        img.style.marginRight = 0;
+    }
 }
 
 img.onclick = function(){
