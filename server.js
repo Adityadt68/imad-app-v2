b566dc2d39fbf90cd0b5b40ce0ed69cf2f83442b
Aditya Narayan Singh <adityanarayansingh065@gmail.com>
2017-02-08 09:06:38 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,13 +5,31 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articleOne = {
+    title : 'Article One | ANS',
+    heading : 'Article One',
+    date : 'sep 5 2015',
+    Contents : `<p>
+                This is the body of Article-One I have specidfied here my body contents of HTML page. This is the body of Article-One I have specidfied here my body contents of HTML page. This is the body of Article-One I have specidfied here my body contents of HTML page. This is the body of Article-One I have specidfied here my body contents of HTML page. This is the body of Article-One I have specidfied here my body contents of HTML page.
+            </p>
+            <p>
+                This is the body of Article-One I have specidfied here my body contents of HTML page. This is the body of Article-One I have specidfied here my body contents of HTML page. This is the body of Article-One I have specidfied here my body contents of HTML page. This is the body of Article-One I have specidfied here my body contents of HTML page. This is the body of Article-One I have specidfied here my body contents of HTML page.
+            </p>
+            <p>
+                This is the body of Article-One I have specidfied here my body contents of HTML page. This is the body of Article-One I have specidfied here my body contents of HTML page. This is the body of Article-One I have specidfied here my body contents of HTML page. This is the body of Article-One I have specidfied here my body contents of HTML page. This is the body of Article-One I have specidfied here my body contents of HTML page.
+            </p>
+            <p>
+                This is the body of Article-One I have specidfied here my body contents of HTML page. This is the body of Article-One I have specidfied here my body contents of HTML page. This is the body of Article-One I have specidfied here my body contents of HTML page. This is the body of Article-One I have specidfied here my body contents of HTML page. This is the body of Article-One I have specidfied here my body contents of HTML page.
+            </p>`
+};
+
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+  res.send(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/article-one', function(req,res){
 
-	res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+	res.send(createTemplate(articleOne));
 });
 
 app.get('/article-second', function(req,res){
@@ -23,6 +41,42 @@ app.get('/article-third', function(req,res){
 
 	res.sendFile(path.join(__dirname, 'ui', 'article-third.html'));
 });
+
+function createTemplate(data){
+   var  bodyContent = data.Contents;
+    var title = data.title;
+    var heading = data.heading;
+    var date = data.date;
+    var htmlTemplate = `<html>
+    <head>
+        <title>
+           ${title}
+        </title>
+        <link href="/ui/style.css" rel="stylesheet" />
+    </head>
+    <body>
+        <div class="container">
+        <div>
+            <a href="/">Home</a>
+        </div>
+        <hr/>
+        <div>
+            ${date}
+        </div>
+        <h3>
+            ${heading}
+        </h3>
+        <div>
+            ${bodyContent}
+        </div>
+        </div>
+    </body>
+    
+</html>`
+
+return htmlTemplate;
+    
+}
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
